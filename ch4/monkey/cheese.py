cd9b4dd73ff312b7f85b83757fc8db8139ea0846
lanzhiwang <hzhilamp@163.com>
2020-05-20 19:22:55 +0800

    pytest monkey

--- ch4/monkey/cheese.py
+++ ch4/monkey/cheese.py
@@ -1,6 +1,16 @@
 import os
 import json
 
+"""
+os.path.expanduser(path)
+把 path 中包含的 "~" 和 "~user" 转换成用户目录
+
+>>> import os
+>>> full_path = os.path.expanduser('~/.cheese.json')
+>>> full_path
+'/Users/huzhi/.cheese.json'
+>>>
+"""
 
 def read_cheese_preferences():
     full_path = os.path.expanduser('~/.cheese.json')
@@ -28,3 +38,28 @@ _default_prefs = {
 
 if __name__ == '__main__':
     write_default_cheese_preferences()
+
+"""
+(venv) huzhi@bogon monkey % python cheese.py
+(venv) huzhi@bogon monkey % ll /Users/huzhi/.cheese.json
+-rw-r--r--  1 huzhi  staff  267  5 20 19:00 /Users/huzhi/.cheese.json
+(venv) huzhi@bogon monkey % cat /Users/huzhi/.cheese.json
+{
+    "slicing": [
+        "manchego",
+        "sharp cheddar"
+    ],
+    "spreadable": [
+        "Saint Andre",
+        "camembert",
+        "bucheron",
+        "goat",
+        "humbolt fog",
+        "cambozola"
+    ],
+    "salads": [
+        "crumbled feta"
+    ]
+}                                                                                                                                                                                                     (venv) huzhi@bogon monkey % rm -rf /Users/huzhi/.cheese.json
+(venv) huzhi@bogon monkey %
+"""
